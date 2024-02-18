377442308c861ac76976c0e72556accc92e80591
IlyaKasiutin <boxfighterilya@gmail.com>
2024-02-18 18:03:32 +0300

    Updated text cleaner - worked too slow

--- text_cleaner.py
+++ text_cleaner.py
@@ -1,25 +1,9 @@
-from nltk.tokenize import word_tokenize
-from textblob import Word
+from autocorrect import Speller
 
 
 class TextCleaner():
-    def __init__(self, confidence: float = 0.85):
-        self.confidence = confidence
-
-    def correct_token(self, token: str) -> str:
-        spellchecker = Word(token.lower())
-        check_result = spellchecker.spellcheck()
-
-        if check_result[0][1] > self.confidence:
-            return check_result[0][0]
-
-        return token
+    def __init__(self):
+        self.checker = Speller(fast=True)
 
     def correct_sentence(self, sentence: str) -> str:
-        tokens = word_tokenize(sentence)
-        processed_tokens = []
-
-        for token in tokens:
-            processed_tokens.append(self.correct_token(token))
-
-        return ' '.join(processed_tokens)
+        return self.checker(sentence)
